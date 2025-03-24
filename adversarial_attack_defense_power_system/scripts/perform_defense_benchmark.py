f00263f08c2f4128c32caf63483e602e9b578f76
cybsbbbb <cybsbbbb@cybsbbbbs-MacBook-Air.local>
2025-03-23 23:25:24 -0700

    Update defense bench

--- adversarial_attack_defense_power_system/scripts/perform_defense_benchmark.py
+++ adversarial_attack_defense_power_system/scripts/perform_defense_benchmark.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import torch
 import numpy as np
@@ -18,19 +19,6 @@ def defense_purification(x_adv, input_transformation_algorithm, transformation_p
     return x_adv_purified
 
 
-# def diffusion_purification(x_adv):
-#     transformation_parameters = {'interconnection': 'b',
-#                                  'timesteps': 20,
-#                                  'beta_type': 'linear',
-#                                  'transformation_type': 'ddim',
-#                                  'diffusion_t': 0.1,
-#                                  'denoise_steps': 3}
-#     x_adv = np.transpose(x_adv, (0, 2, 3, 1))
-#     x_adv_purified = diffusion_dataset_transformation(x_adv, transformation_parameters)
-#     x_adv_purified = np.transpose(x_adv_purified, (0, 3, 1, 2))
-#     return x_adv_purifieds
-
-
 def diffusion_purification_benchmark_black(interconnection, model_name, attack_algorithm,
                                            input_transformation_algorithm, transformation_parameters, device):
     setup_random_seeds(428)
@@ -46,17 +34,27 @@ def diffusion_purification_benchmark_black(interconnection, model_name, attack_a
     # Load the trained classifier
     net = load_classifier(interconnection=interconnection, model_name=model_name, device=device)
     # Diffusion adversarial purification
-    x_adv_purified = diffusion_purification(x_adv)
+    x_adv_purified = defense_purification(x_adv, input_transformation_algorithm, transformation_parameters)
     # Save purified result
     np.save(f'{result_sub_dir}/x_adv_purified.npy', x_adv_purified)
-
-    f1_before = evaluation_numpy(x_adv, testset.label, net, device)
-    print(f"F1 before: {f1_before:}")
-
+    # Evaluate the F1 score
     f1_after = evaluation_numpy(x_adv_purified, testset.label, net, device)
     print(f"F1 after: {f1_after}")
-
     return f1_before, f1_after
+
+
+# def diffusion_purification(x_adv):
+#     transformation_parameters = {'interconnection': 'b',
+#                                  'timesteps': 20,
+#                                  'beta_type': 'linear',
+#                                  'transformation_type': 'ddim',
+#                                  'diffusion_t': 0.1,
+#                                  'denoise_steps': 3}
+#     x_adv = np.transpose(x_adv, (0, 2, 3, 1))
+#     x_adv_purified = diffusion_dataset_transformation(x_adv, transformation_parameters)
+#     x_adv_purified = np.transpose(x_adv_purified, (0, 3, 1, 2))
+#     return x_adv_purifieds
+
 
 
 if __name__ == '__main__':
@@ -70,31 +68,48 @@ if __name__ == '__main__':
     print(f"Using Device: {device}")
     # device = torch.device("cpu")
 
+    script_dir = Path(__file__).resolve().parent
+
     interconnection_list = ['b', 'c']
     model_name_list = ['vgg13', 'mobilenet_v2', 'densenet121', 'resnet18', 'resnet50']
     attack_algorithms = ['simba_attack', 'zo_sign_sgd_attack', 'sign_hunter_attack',
                          'boundary_attack', 'opt_attack', 'sign_opt_attack',
-                         'bit_schedule_v6', 'bit_schedule_transfer_v2'][-2:]
+                         'bit_schedule_v4', 'bit_schedule_v6',
+                         'bit_schedule_transfer_v1', 'bit_schedule_transfer_v2']
+    input_transformation_algorithms = ['', 'spatial_smoothing', 'low_pass_filtering', 'feature_squeezing',
+                                       'svd_decomposition', 'event_decomposition', 'diffusion']
+
 
     for interconnection in interconnection_list[:1]:
-        script_dir = Path(__file__).resolve().parent
-        result_file_path = f'{script_dir}/../../adv_attack_result/black/{interconnection}_result_table_diffusion_purification.csv'
+        result_file_dir = f"./../../adv_exp_result/analysis/defense_benchmark"
+        if not os.path.exists(result_file_dir):
+            os.makedirs(result_file_dir)
+        result_file_path = f'{result_file_dir}/{interconnection}_result_table_defense_benchmark.csv'
         with open(result_file_path, 'w', newline='') as csvfile:
-            fieldnames = ['model_name']
+            fieldnames = ['exp_name']
             for attack_algorithm in attack_algorithms:
                 fieldnames.append(f'{attack_algorithm}')
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             for model_name in model_name_list[:]:
-                row_before = {'model_name': model_name + "_before", }
-                row_after = {'model_name': model_name + "_after", }
-                for attack_algorithm in attack_algorithms[:]:
-                    f1_before, f1_after = diffusion_purification_benchmark_black(interconnection=interconnection,
-                                                                                 model_name=model_name,
-                                                                                 attack_algorithm=attack_algorithm,
-                                                                                 device=device)
-                    row_before[attack_algorithm] = f1_before
-                    row_after[attack_algorithm] = f1_after
-                    torch.cuda.empty_cache()
-                writer.writerow(row_before)
-                writer.writerow(row_after)
+                for input_transformation_algorithm in input_transformation_algorithms[:]:
+                    exp_name = f'ic_{interconnection}_{model_name}_{input_transformation_algorithm}'
+                    row = {'exp_name': exp_name}
+                    transformation_parameters = ""
+                    if input_transformation_algorithm == 'diffusion':
+                        transformation_parameters = {'interconnection': interconnection,
+                                                     'timesteps': 20,
+                                                     'beta_type': 'linear',
+                                                     'transformation_type': 'ddim',
+                                                     'diffusion_t': 0.1,
+                                                     'denoise_steps': 3}
+                    for attack_algorithm in attack_algorithms[:]:
+                        f1_after = diffusion_purification_benchmark_black(interconnection=interconnection,
+                                                                          model_name=model_name,
+                                                                          attack_algorithm=attack_algorithm,
+                                                                          input_transformation_algorithm=input_transformation_algorithm,
+                                                                          transformation_parameters=transformation_parameters,
+                                                                          device=device)
+                        row[attack_algorithm] = f1_after
+                        torch.cuda.empty_cache()
+                    writer.writerow(row)
